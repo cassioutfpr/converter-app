c4769106110fb9350d8089b090ffbc98b6696220
Cassio Henrique Garcia Morales <cassiomorales@cassios-macbook-pro.local>
2016-05-24 14:47:50 -0400

    Initial Commit

---
+++ ConverterTests/ConverterTests.swift
@@ -0,0 +1,36 @@
+//
+//  ConverterTests.swift
+//  ConverterTests
+//
+//  Created by Cassio Henrique Garcia Morales on 5/24/16.
+//  Copyright © 2016 Cassio Henrique Garcia Morales. All rights reserved.
+//
+
+import XCTest
+@testable import Converter
+
+class ConverterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
